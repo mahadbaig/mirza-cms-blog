8713dadd9a6e1f5c0ad211cf219f75b1f31a5980
Mirza Mahad Baig <mahadmirza681@gmail.com>
2023-03-05 23:42:20 +0500

    Home page JSX almost done

---
+++ Components/PostCard.tsx
@@ -0,0 +1,13 @@
+import { title } from 'process'
+import React from 'react'
+
+const PostCard = ({ post }:any) => {
+  return (
+    <div>
+        {post.title}
+        {post.excerpt}
+    </div>
+  )
+}
+
+export default PostCard
